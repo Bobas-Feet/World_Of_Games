d31d61997c833c005c5b50cdf56a684d234e85ad
Bar <bar.mustacchi@gmail.com>
2023-02-25 16:19:04 +0200

    added new MainScores.py file

---
+++ MainScores.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from pathlib import Path
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def score_server():
+    try:
+        score_file = open(Path("Scores.txt"), "r")
+        score = score_file.read()
+        return render_template('score.html', SCORE=score)
+    except FileNotFoundError or FileExistsError:
+        return render_template('error.html', ERROR='Unknown Error')
+
+
+app.run()
