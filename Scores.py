ea59d5db7a86ff49d4c3cea3b777e316eca7d87b
Bar <bar.mustacchi@gmail.com>
2023-02-26 22:50:12 +0200

    Updated Scores.py and Utils.py files

--- Scores.py
+++ Scores.py
@@ -1,28 +1,21 @@
-from Utils import user_score
+from pathlib import Path
 
 
 def add_score(difficulty):
 
     points_for_winning = str((difficulty * 3) + 5)
-    user_score(points_for_winning)
-    print(f"Score: {points_for_winning}")
+    file_path = 'C:/DevOps/WoG (project)/'
+    try:
+        score_file = open(Path('Scores.txt'), 'r')
+        score = open(Path('Scores.txt'), 'a')
+        score.write(points_for_winning)
+        if score_file.read() == 'r':
+            contents = score_file.read()
+            print(contents)
 
+    except FileNotFoundError:
+        scores_file_name = open(f'{file_path}/Scores.txt', 'x')
+        scores_file_name.write(points_for_winning)
 
-    # file_path = 'C:/DevOps/WoG (project)/'
-    # try:
-    #     # score = open('C:/DevOps/WoG (project)/Scores.txt', 'w')
-    #     # score_file = open("Scores.txt", "x")
-    #     scores_file_name = open("Scores.txt", "w+")
-    #     scores_file_name.write(points_for_winning)
-    #
-    #     scores_file_name = open("Scores.txt", "r")
-    #     if scores_file_name.read() == "r":
-    #         contents = scores_file_name.read()
-    #         print(contents)
-    #
-    # except FileNotFoundError:
-    #     scores_file_name = open(f"{file_path}/Scores.txt", "x")
-    #     scores_file_name.write(points_for_winning)
-
-    if __name__ == "__main__":
-        print(f"Score: {points_for_winning}")
+    if __name__ == '__main__':
+        print(f'Score: {points_for_winning}')
--- Utils.py
+++ Utils.py
@@ -1,16 +1,6 @@
-# from pathlib import Path
+from pathlib import Path
 
-
-def user_score(points_for_winning):
-    scores_file_name = open('Scores.txt', 'w+')
-    scores_file_name.write(points_for_winning)
-    scores_file_name.close()
-
-    scores_file_name = open('Scores.txt', 'r+')
-    scores = scores_file_name.read()
-    scores_file_name.close()
-    return scores
-
+score_file_name = open(Path('score.txt'), 'x')
 
 bad_return_code = int(401)
 
