cbd25fa6ca207c9ad1f7e44d890cc3b054d0902a
Bar <bar.mustacchi@gmail.com>
2023-03-01 00:09:59 +0200

    Updated CurrencyRouletteGame.py

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,11 +1,11 @@
 # 24/2/2023
 
 from forex_python.converter import CurrencyRates
-# import json
+import json
 import os
-# import urllib.request as r
+import urllib.request as r
 import random
-# from credentials import api_key
+from credentials import api_key
 
 
 def get_money_interval(difficulty):
@@ -13,20 +13,20 @@ def get_money_interval(difficulty):
     # url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/USD/ILS'
     # response = r.urlopen(url)
     # data = json.load(response)
+    # ex = int(data["USD_ILS"])
 
     cr = CurrencyRates()
     cr.get_rates('USD')
-    # ex = int(data["USD_ILS"])
-    rng = int(random.uniform(1, 100))
-    print(f'To play this game you are given ${rng}, YAY.')
 
+    rng = int(random.uniform(1, 100))
     from_currency = 'EUR'
     to_currency = 'USD'
     ex = cr.convert(from_currency, to_currency, 1)
-    print(ex)
+    print(f'The Exchange rate for today is {ex}')
     t = rng * ex
     low = int(t - (5 - difficulty))
     high = int(t + (5 - difficulty))
+    print(f'To play this game you are given ${rng}, YAY.')
     return rng, t, low, high
 
 
