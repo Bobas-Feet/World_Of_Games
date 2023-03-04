6cd2f3f63eb79f66ea256cdce78b444e2bc84a05
Bar <bar.mustacchi@gmail.com>
2023-03-04 23:27:35 +0200

    Updated CurrencyRouletteGame.py

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,4 +1,3 @@
-# from threading import Timer
 from inputimeout import inputimeout
 import requests
 import datetime
@@ -16,19 +15,20 @@ def get_money_interval(difficulty):
     data = response.json()
     converted = data['conversion_rate']
 
-    print(f'The exchange rate for today, {today}, is: \n{converted} ILS = 1 USD')
+    print(f'The exchange rate for today, {today}, is: \n1 USD = {converted} ILS')
     rng = int(random.uniform(1, 101))
     print(f'You get ${rng}, YAY.')
-    interval = (rng - (5 - difficulty), rng + (5 - difficulty))
+    # low = int(rng - (5 - difficulty))
+    # high = int(rng + (5 - difficulty))
+    # interval = (rng - (5 - difficulty), rng + (5 - difficulty))
 
-    return interval
+    return rng
 
 
 def get_guess_from_user(rng):
 
     while True:
         try:
-
             guess = int(inputimeout(prompt=f'Can you calculate how much ${rng}'
                                            f' is in ILS? \nYou have 10 seconds to answer: ', timeout=10))
             if guess > rng:
@@ -41,15 +41,20 @@ def get_guess_from_user(rng):
         except ValueError:
             print("Error: You can only enter numbers.")
         except Exception:
-            timed_out = 'Timed out'
+            timed_out = 'Too slow. You timed out.'
             print(timed_out)
             quit()
 
 
 def play(difficulty):
 
-    interval = get_money_interval(difficulty)
-    get_guess_from_user(interval)
+    rng = get_money_interval(difficulty)
+    if get_guess_from_user(rng):
+        print('You got it right, you are a lean-mean-calculating machine.')
+        quit()
+    else:
+        print('Well, Fuck. You lost.')
+        quit()
 
 
 
