c9d245ef2c8c909e69959b8617804ab13d090330
Bar <bar.mustacchi@gmail.com>
2023-03-04 19:53:01 +0200

    Updated CurrencyRouletteGame.py

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,23 +1,24 @@
 import requests
-# from openexchangerate import OpenExchangeRates
 import datetime
-import json
-# import os
 import random
 from credentials import api_key
 
 
 def get_money_interval(difficulty):
+
     now = datetime.datetime.now()
-    AMOUNT = 1
-    url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/USD/ILS/{AMOUNT}'
+    amount = 1
+    url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/USD/ILS/{amount}'
+
     response = requests.get(url)
     data = response.json()
     converted = data['conversion_rate']
+
     print(f'The exchange rate for today, {now}, is {converted}')
     rng = int(random.uniform(1, 101))
     print(f'You get ${rng}, YAY.')
     interval = (rng - (5 - difficulty), rng + (5 - difficulty))
+
     return interval
 
 
@@ -33,7 +34,9 @@ def get_guess_from_user(rng):
 
 
 def play(difficulty):
-    rng = get_money_interval(difficulty)
-    get_guess_from_user(rng)
+
+    interval = get_money_interval(difficulty)
+    get_guess_from_user(interval)
+
 
 
