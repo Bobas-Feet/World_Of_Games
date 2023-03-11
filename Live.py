203b7841493d205803d04502ff1cdf4227746fcf
Bar <bar.mustacchi@gmail.com>
2023-03-11 23:41:01 +0200

    Updated Live.py

--- Live.py
+++ Live.py
@@ -2,12 +2,13 @@ import GuessGame
 import MemoryGame
 import CurrencyRouletteGame
 from Scores import add_score
-import MainScores
+# import MainScores
 
 
 def welcome():
-    name = input('What is your name? [You may use only letters,'
-                 '\nno numbers or special symbols (!@#$%^) and no spaces]\n').lower()
+    name = input('Greetings newcomer, what is your name?\n'
+                 '[No numbers [0-9], no special symbols [_!@#$%^] and no spaces,'
+                 ' you may use only letters.]\n').lower()
 
     try:
         if any([i > 'z' or i < 'a' for i in name]):
@@ -46,7 +47,7 @@ def load_game():
                     print('Your only options here are [1/2/3/4/5]')
                 elif bool(MemoryGame.play(difficulty)) is True:
                     add_score(difficulty=difficulty)
-                    MainScores.score_server()
+                    # MainScores.score_server()
 
             if int(g) == 2:
                 print(f'{game2}: A numbers guessing game in which you need to guess the number that'
@@ -56,7 +57,7 @@ def load_game():
                     print('Your only options here are [1/2/3/4/5]')
                 elif bool(GuessGame.play(difficulty)) is True:
                     add_score(difficulty=difficulty)
-                    MainScores.score_server()
+                    # MainScores.score_server()
 
             if int(g) == 3:
                 print(f'{game3}: Try and guess the value of a random'
@@ -66,7 +67,7 @@ def load_game():
                     print('Your only options here are [1/2/3/4/5]')
                 elif bool(CurrencyRouletteGame.play(difficulty)) is True:
                     add_score(difficulty=difficulty)
-                    MainScores.score_server()
+                    # MainScores.score_server()
 
             while int(g) == 0:
                 quit_game = input('Are you sure you want to quit? [y/n]\n').lower()
